e009308267acf9965186e441df1caf331a0f303a
Polina <paulinapytaleva@yandex.by>
2024-01-08 15:05:46 +0300

    лшнплш

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,7 +1,4 @@
 package task1;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
